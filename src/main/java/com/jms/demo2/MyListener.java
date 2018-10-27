0276a4012649d0e1d038ccfedea4c93e9d5c2b05
Igor Rudenko <fac30ff@gmail.com>
2018-10-27 12:41:56 +0300

    first comit

---
+++ src/main/java/com/jms/demo2/MyListener.java
@@ -0,0 +1,17 @@
+package com.jms.demo2;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class MyListener implements MessageListener {
+  public void onMessage(Message message) {
+    try {
+      TextMessage msg = (TextMessage) message;
+      System.out.println("following message is received:" + msg.getText());
+    } catch (JMSException e) {
+      System.out.println(e);
+    }
+  }
+}
